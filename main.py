421a9a52a80d6a23f16aae26848e34f34933fbf5
supriya201 <33290508+supriya201@users.noreply.github.com>
2020-09-17 19:56:17 +0530

    Update main.py
    printed new line

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print "Hello World"
     print "hello world with stars"
+    print "who printed this??"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
