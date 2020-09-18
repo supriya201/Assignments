f334972ad5fa45d40c9f24abb55ce59645d0321b
supriya201 <padamwarsupriya@gmail.com>
2020-09-18 07:07:06 +0530

    Delete formid and formsubmissionid #3 Number is not sorted #2 Number column sorted and deleted forid and formsubmission id for each entry

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-# This is a sample Python script.
+import assignment
+import datetime
+data= [{"FormSubmissionID":68223,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data123","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"06\/01\/2021","Numbers":"198"},{"FormSubmissionID":68222,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"08\/01\/2020","Numbers":"3"},{"FormSubmissionID":68221,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"02\/01\/2021","Numbers":"156"},{"FormSubmissionID":68220,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"11\/01\/2020","Numbers":"135"},{"FormSubmissionID":68219,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"01\/01\/2021","Numbers":"148"},{"FormSubmissionID":68218,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"04\/01\/2021","Numbers":"175"},{"FormSubmissionID":68217,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"10\/01\/2020","Numbers":"120"},{"FormSubmissionID":68216,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"03\/01\/2021","Numbers":"165"},{"FormSubmissionID":68215,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Forecast","Month and Year":"05\/01\/2021","Numbers":"186"},{"FormSubmissionID":68214,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Planned","Month and Year":"07\/01\/2020","Numbers":"1"},{"FormSubmissionID":68213,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Actual","Month and Year":"09\/01\/2020","Numbers":"6"},{"FormSubmissionID":68212,"FormID":"54792","CFT":"Sourcing","Group":"Testing Reports","Category":"Testing Report Data","Sub-Category":"Testing Report Data Bot","Initiative":"Bot for Testing Report Data","Type":"Planned","Month and Year":"12\/01\/2020","Numbers":"141"}]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+assignment.date_format(data,"Month and Year")
 
+assignment.sumOfNum(data,"Numbers")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print "Hello World"
-    print "hello world with stars"
-    print "who printed this??"
-    print "hello world second time"
-    print "Trying pull"
+assignment.cummmu_sum(data,"Numbers")
+assignment.deleteValues(data,"FormID")
+assignment.deleteValues(data,"FormSubmissionID")
+assignment.concat_values(data,"Category","Sub-Category")
+assignment.type_entry(data,"Type","Actual")
+assignment.type_entry(data,"Type","Forecast")
+print assignment.get_number(data)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for row in data:
+    date1 = datetime.date(2020,01,01)
+    date2 = (datetime.datetime.strptime(row["Month and Year"],"%m\/%d\/%Y")).date()
+    assignment.no_of_days(date1, date2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
